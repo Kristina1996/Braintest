798fd722f10eec5498c9abbe51aa034ee5fd551b
Kristina Baskakova <kristina.baskakova15@mail.ru>
2018-06-06 23:35:59 +0300

    test
    test

---
+++ app/src/main/java/com/example/krist/braintest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.krist.braintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    EditText editTextEmail, editTextPassword;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
